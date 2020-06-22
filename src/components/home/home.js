8438a2a7c0463374aab904dfad7efdefe08d4329
vipul goyal <vipulgoyal112@gmail.com>
2020-06-22 07:19:12 +0530

    optional-chaning component added

---
+++ src/components/home/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Link
+} from "react-router-dom";
+import './home.css';
+
+function Home() {
+  return (
+    <div>
+        <h1>Ecmascript 2020 or ES11 - Topics</h1>
+        <ul className="fl">
+            <li>
+            <Link to="/optional-chaining">Optional Chaining</Link>
+            </li>
+        </ul>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/optional-chaining/optional-chaining.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './optional-chaining.css';
+
+function OptionalChaining() {
+  return (
+    <div>
+      <h1>Optional Chaining</h1>
+    </div>
+  );
+}
+
+export default OptionalChaining;
